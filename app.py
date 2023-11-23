f9f46bb89e5bc15b5f8670235cf215d9eab91a29
Prakash-Kani <prakashkoffi@gmail.com>
2023-11-23 22:37:30 +0530

    Updated analysis figure in dashboard

--- app.py
+++ app.py
@@ -34,12 +34,28 @@ col4, col5, col6=st.columns([2,2,2], gap = 'small')
 with col4:
     fig = main.Geo_fig(country, suburb, bed_rooms)
     st.plotly_chart(fig)
+
+    st.plotly_chart(main.top_10_suburb_properties(country, suburb, bed_rooms))
+
+    st.plotly_chart(main.min_nights_properties(country, suburb, bed_rooms))
+
+    st.plotly_chart(main.max_nights_properties(country, suburb, bed_rooms))
 with col5:
+    st.plotly_chart(main.bed_price_pie(country, suburb, bed_rooms))
     bed_price_fig =main.Bed_price(country, suburb, bed_rooms)
 
     st.plotly_chart(bed_price_fig)
-# st.write(bed_price_fig)
+
+    st.plotly_chart(main.property_type_fig(country, suburb, bed_rooms))
+
+    st.plotly_chart(main.host_reviews_fig(country, suburb, bed_rooms))
 with col6:
     room_price_fig =main.room_price(country, suburb, bed_rooms)
 
     st.plotly_chart(room_price_fig)
+
+    st.plotly_chart(main.top_10_host_properties(country, suburb, bed_rooms))
+
+    st.plotly_chart(main.cancellation_fig(country, suburb, bed_rooms))
+
+    st.plotly_chart(main.guests_price_fig(country, suburb, bed_rooms))
